62f84d3556e1ae4958c02fa7b4faf941a0a3e596
Jasper <jasper-d@users.noreply.github.com>
2019-01-28 13:54:32 +0100

    Add test for parsing IPv6 addresses

--- src/tests/UnitTests/NatsServerInfoParseTests.cs
+++ src/tests/UnitTests/NatsServerInfoParseTests.cs
@@ -7,6 +7,14 @@ namespace UnitTests
     public class NatsServerInfoParseTests : UnitTests
     {
         [Fact]
+        public void Should_be_able_to_parse_server_info_with_ipv6_addresses()
+        {
+            var serverInfo = "{\"server_id\":\"vvm7f5eDAgSJNSeLeDcF5Y\",\"version\":\"1.4.0\",\"proto\":1,\"go\":\"go1.11.4\",\"host\":\"0.0.0.0\",\"port\":4222,\"auth_required\":true,\"max_payload\":30000000,\"client_id\":17,\"connect_urls\":[\"42.47.11.11:4222\",\"[2001:0:47ef:15de:421:2eef:f500:fcf4]:4222\",\"42.47.11.22:4222\",\"[2001:0:47ef:15de:74:f83f:d78c:d4e3]:4222\",\"42.47.11.21:4222\",\"[2001:0:47ef:15de:30fa:f43f:d78c:d4e3]:4222\"]}";
+
+            NatsServerInfo.Parse(serverInfo);
+        }
+    
+        [Fact]
         public void Should_be_able_to_parse_server_info_When_protocol_0_data_is_returned()
         {
             var parsed = NatsServerInfo.Parse("{\"server_id\":\"Vwp6WDR1NIEuFr0CQ9PtMa\",\"version\":\"0.8.0\",\"go\":\"go1.6.2\",\"host\":\"0.0.0.0\",\"port\":4222,\"auth_required\":false,\"ssl_required\":false,\"tls_required\":false,\"tls_verify\":false,\"max_payload\":1048576}");
